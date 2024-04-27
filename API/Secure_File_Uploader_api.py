9554cb8656d99e8c81853a8b007b14100df236e0
celinec1 <cchen@bu.edu>
2024-04-27 13:25:04 -0400

    only extention will be pdf and txt

--- API/Secure_File_Uploader_api.py
+++ API/Secure_File_Uploader_api.py
@@ -9,7 +9,7 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client["Smart_Doc"]
 fs = gridfs.GridFS(db)
 
-ALLOWED_EXTENSIONS = {'pdf', 'txt', 'png', 'jpeg'}
+ALLOWED_EXTENSIONS = {'pdf', 'txt'}
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
