c96a11408abf4b30e9739b0f92df532bea67a61d
celinec1 <cchen@bu.edu>
2024-04-30 16:17:59 -0400

    added login invalid test

--- test_app.py
+++ test_app.py
@@ -24,3 +24,21 @@ def test_valid_login(mock_get_db):
     # Assertions to verify expected outcome
     assert status_code == 200
     assert 'Authentication approved' in message
+
+def test_invalid_login(mock_get_db):
+    """ Test login with valid credentials """
+    mock_client = mongomock.MongoClient()
+    mock_db = mock_client.db
+    mock_get_db.return_value = mock_db
+
+    mock_users = mock_db.users
+    mock_users.insert_one({'username': 'celine', 'password': bcrypt.hashpw('testing'.encode('utf-8'), bcrypt.gensalt())})
+
+    username = 'celine'
+    password = 'hello'
+    message, status_code = login(username, password)
+
+    assert status_code == 401
+    assert 'Authentication not approved' in message
+
+
